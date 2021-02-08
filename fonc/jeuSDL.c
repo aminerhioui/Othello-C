22c43cf5c654980c07ab067a78ad4a3c471b39ad
aminerhioui <mohammedaminerhioui@gmail.com>
2021-02-09 00:50:59 +0100

    Othello C

---
+++ fonc/jeuSDL.c
@@ -0,0 +1,16 @@
+
+#include "SDLfonction.h"
+// #include "logique_jeu.h"
+// #include "fonction_Tritement_fichier.h"
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+
+    Game();
+    
+    return EXIT_SUCCESS;
+}
